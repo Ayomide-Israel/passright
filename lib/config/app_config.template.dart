e265e944fab3a1aa76b46b184d6cdf82130d62ec
Ayomide-Israel <apraiseno152@gmail.com>
2025-09-28 16:08:25 +0100

    remove API keys

--- lib/config/app_config.template.dart
+++ lib/config/app_config.template.dart
@@ -1,6 +1,3 @@
 class AppConfig {
-  static const String apiKey = 'YOUR_API_KEY_HERE';
+  static const String apiKey = 'YOUR_HUGGINGFACE_API_KEY_HERE';
 }
-
-// Add to .gitignore
-lib/config/app_config.dart
